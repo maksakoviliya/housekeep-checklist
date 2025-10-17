bd80e244936dedc36092210bcf843bf0d71593dd
maksak_il <maksak_il@mail.ru>
2025-10-17 23:45:46 +0300

    Create checklist model

---
+++ database/migrations/2025_10_17_203953_create_checklists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Property;
+use App\Models\Room;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->id();
+			$table->foreignIdFor(Task::class)->constrained()->cascadeOnDelete();
+			$table->foreignIdFor(Property::class)->constrained()->cascadeOnDelete();
+			$table->foreignIdFor(Room::class)->constrained()->cascadeOnDelete();
+	        $table->foreignIdFor(User::class)->nullable()->constrained()->nullOnDelete();
+			$table->dateTime('started_at')->nullable();
+			$table->dateTime('finished_at')->nullable();
+			$table->boolean('is_checked')->default(false);
+			$table->text('notes')->nullable();
+			$table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('checklists');
+    }
+};
